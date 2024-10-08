22ef4e24d7c77468ba328e4871dcf9d86b0abc13
Aníbal Uriel <anibal.u.sepulveda@gmail.com>
2024-10-08 17:34:56 -0300

    audiopy initial integration

--- IRCwidget/catcher.py
+++ IRCwidget/catcher.py
@@ -1,27 +1,26 @@
+
 import wave
 import sys
+
 import pyaudio
+
 
 CHUNK = 1024
 
-def play_audio_file(filename):
-    with wave.open(filename, 'rb') as wf:
-        p = pyaudio.PyAudio()
-        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                        channels=wf.getnchannels(),
-                        rate=wf.getframerate(),
-                        output=True)
+if len(sys.argv) < 2:
+    print(f'Plays a wave file. Usage: {sys.argv[0]} filename.wav')
+    sys.exit(-1)
 
-        while len(data := wf.readframes(CHUNK)):
-            stream.write(data)
+with wave.open(sys.argv[1], 'rb') as wf:
+    p = pyaudio.PyAudio()
 
-        stream.close()
-        p.terminate()
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print(f'Plays a wave file. Usage: {sys.argv[0]} <audio_file>')
-        sys.exit(-1)
+    while len(data := wf.readframes(CHUNK)): 
+        stream.write(data)
 
-    filename = sys.argv[1]
-    play_audio_file(filename)
+    stream.close()
+    p.terminate()
