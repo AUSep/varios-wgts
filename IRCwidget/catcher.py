0bcf70e3602aaeb3d99c2e4fdc8177e5f08c3d95
Aníbal Uriel <anibal.u.sepulveda@gmail.com>
2024-10-10 21:10:53 -0300

    record

--- IRCwidget/catcher.py
+++ IRCwidget/catcher.py
@@ -1,26 +1,40 @@
-
 import wave
 import sys
-
 import pyaudio
 
-
 CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1 if sys.platform == 'darwin' else 2
+RATE = 44100
+RECORD_SECONDS = 5
 
-if len(sys.argv) < 2:
-    print(f'Plays a wave file. Usage: {sys.argv[0]} filename.wav')
-    sys.exit(-1)
+def play_audio(dir : str) -> None:
+    with wave.open(dir, 'rb') as wf:
+        p = pyaudio.PyAudio()
 
-with wave.open(sys.argv[1], 'rb') as wf:
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True)
+
+        while len(data := wf.readframes(CHUNK)):
+            stream.write(data)
+
+    stream.close()
+    p.terminate()
+
+with wave.open('output.wav', 'wb') as wf:
     p = pyaudio.PyAudio()
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
 
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    output=True)
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True)
 
-    while len(data := wf.readframes(CHUNK)): 
-        stream.write(data)
+    print('Recording...')
+    for _ in range(0, RATE // CHUNK * RECORD_SECONDS):
+        wf.writeframes(stream.read(CHUNK))
+    print('Done')
 
     stream.close()
     p.terminate()
--- IRCwidget/mainapp.py
+++ IRCwidget/mainapp.py
@@ -1,14 +1,17 @@
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QPushButton, QMainWindow, QGridLayout, QDialog, QFileDialog, QWidget, QLineEdit
+from PyQt6.QtWidgets import QApplication, QPushButton, QMainWindow, QGridLayout, QFileDialog, QWidget, QLineEdit
 import sys
 from app_models import ImpResponses
+import catcher as ctch
+
 
 class MainWin(QMainWindow):
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         self.setWindowTitle('Ventana principal')
-        self.setGeometry(100,100,500,300)
+        self.setGeometry(100, 100, 500, 300)
         mainwdgt = QWidget(self)
         layout = QGridLayout()
         mainwdgt.setLayout(layout)
@@ -25,36 +28,45 @@ class MainWin(QMainWindow):
         file_menu.addAction('Exit', self.destroy)
 
         #File browser and directory entry
+        
+        self.dir_entry = QLineEdit()
+        layout.addWidget(self.dir_entry, 0, 0)
+
         btn_layout = QGridLayout()
         file_browser_btn = QPushButton('Browse')
         file_browser_btn.clicked.connect(self.open_file_dlg)
-        btn_layout.addWidget(file_browser_btn, 0,0)
+        btn_layout.addWidget(file_browser_btn, 0, 0)
 
         save_file_btn = QPushButton('Save in db')
         save_file_btn.clicked.connect(self.save_file)
-        btn_layout.addWidget(save_file_btn,0,1)
-        layout.addLayout(btn_layout,1,0)
-        
-        self.dir_entry=QLineEdit()
-        layout.addWidget(self.dir_entry, 0, 0)
+        btn_layout.addWidget(save_file_btn, 0, 1)
+
+        play_file_btn = QPushButton('Play file')
+        play_file_btn.clicked.connect(self.play)
+        btn_layout.addWidget(play_file_btn,0,2)
+        layout.addLayout(btn_layout, 1, 0)
 
         self.show()
 
+    def play(self) -> None:
+        file_path = self.dir_entry.text()
+        ctch.play_audio(file_path)
+
     def open_file_dlg(self) -> None:
-        dlg=QFileDialog(self)
+        dlg = QFileDialog(self)
         dlg.setFileMode(QFileDialog.FileMode.ExistingFiles)
         dlg.setViewMode(QFileDialog.ViewMode.Detail)
         if dlg.exec():
-            file_to_get=dlg.selectedFiles()
+            file_to_get = dlg.selectedFiles()
             if file_to_get:
-                file_to_get=file_to_get[0]
+                file_to_get = file_to_get[0]
                 self.dir_entry.setText(file_to_get)
-    
+
     def save_file(self) -> None:
         file_path = self.dir_entry.text()
         with open(file_path, 'rb') as file:
-            binary_file=file.read()
-        q = ImpResponses.insert(response_file = binary_file)
+            binary_file = file.read()
+        q = ImpResponses.insert(response_file=binary_file)
         q.execute()
         print(q)
         self.dir_entry.setText('')
