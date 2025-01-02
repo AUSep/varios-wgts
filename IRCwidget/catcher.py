c42b16f3bca5a12db6a9bfe8545022b6adedddb3
Aníbal Uriel <anibal.u.sepulveda@gmail.com>
2025-01-02 17:03:43 -0300

    test refactorized

--- IRCwidget/catcher.py
+++ IRCwidget/catcher.py
@@ -1,24 +1,9 @@
-import wave
-import sys
-import pyaudio
+d1={'gat':56,'hy':543,'hdak':64}
+d2={'gat':345,'hy':5165,'hdak':5}
+d3={'gat':5245,'hy':89,'hdak':6}
+d_tupl=(d1,d2,d3)
+def get_gat(*d_tupl:dict) -> None:
+    gat = [d['gat'] for d in d_tupl]
+    return gat
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1 if sys.platform == 'darwin' else 2
-RATE = 44100
-RECORD_SECONDS = 5
-
-def play_audio(dir : str) -> None:
-    with wave.open(dir, 'rb') as wf:
-        p = pyaudio.PyAudio()
-
-        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                        channels=wf.getnchannels(),
-                        rate=wf.getframerate(),
-                        output=True)
-
-        while len(data := wf.readframes(CHUNK)):
-            stream.write(data)
-
-    stream.close()
-    p.terminate()
+print(get_gat(d1,d2,d3))
--- IRCwidget/test.py
+++ IRCwidget/test.py
@@ -3,6 +3,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
+def signal_data(file_dir : str, label: str) -> dict:
+    with wave.open(file_dir, 'rb') as wf:
+        channels = wf.getnchannels()
+        if channels == 1:
+            samp_rate = wf.getframerate()
+            samples = wf.getnframes()
+            dtype = get_dtype(wf)
+            time = np.arange(samples)/samp_rate
+            freqs = np.fft.fftfreq(n=samples, d=1/samp_rate)
+            audio_data : bytes = wf.readframes(samples)
+            wf_array : np.ndarray = np.frombuffer(audio_data, dtype)
+            fft_data=np.fft.fft(wf_array)
+            spectrum=np.abs(fft_data)
+            colour = get_random_colour()
+            audio_data_dict : dict = {'wave_form' : (wf_array.tolist(),label, colour),
+                                      'spectrum' : (spectrum.tolist(),label, colour),
+                                      'time' : time.tolist(),
+                                      'frequence' : freqs.tolist()}
+            return audio_data_dict
+        else:
+            raise Exception('The audio file should be mono')
+        
+def plot_data(*data_d : dict) -> None:
+    w_arr = [data['wave_form'] for data in data_d]
+    s_arr = [data['spectrum'] for data in data_d]
+    t_arr = [data['time'] for data in data_d]
+    t_arr = max(t_arr)
+    f_arr = [data['frequence'] for data in data_d]
+    f_arr = max(f_arr)
+    for w in w_arr:
+        arr = resize_arrays(w[0], len(t_arr))
+        label = w[1]
+        colour = w[2]
+        plt.plot(t_arr, arr, colour, label = label)
+    plt.legend()
+    plt.show()
+    for s in s_arr:
+        arr = resize_arrays(s[0], len(f_arr))
+        label = w[1]
+        colour = w[2] 
+        plt.plot(f_arr, arr, colour, label = label)
+    plt.legend()
+    plt.show()
+        
 def get_dtype(wf : wave.Wave_read) -> np.signedinteger:
     samp_width = wf.getsampwidth()
     if samp_width == 1:
@@ -13,76 +57,13 @@ def get_dtype(wf : wave.Wave_read) -> np.signedinteger:
         dtype = np.int32
     return dtype
 
-def signal_plot_data(file_dir : str) -> dict[str, list]:
-    with wave.open(file_dir, 'rb') as wf:
-        channels = wf.getnchannels()
-        samp_rate = wf.getframerate()
-        samples = wf.getnframes()
-        dtype = get_dtype(wf)
-        time: np.ndarray = np.arange(samples)/samp_rate
-        freqs=np.fft.fftfreq(n=samples, d=1/samp_rate)
-        if channels == 1:
-            audio_data : bytes = wf.readframes(samples)
-            wf_array : np.ndarray = np.frombuffer(audio_data, dtype)
-            fft_data=np.fft.fft(wf_array)
-            spectrum=np.abs(fft_data)
-            freqs=np.fft.fftfreq(n=samples, d=1/samp_rate)
-            audio_data_dict : dict = {'wave_form' : wf_array.tolist(),
-                                      'seconds' : time.tolist(),
-                                      'spectrum' : spectrum.tolist(),
-                                      'frequence' : freqs.tolist()}
-            return audio_data_dict
-        else:
-            raise Exception('The audio file should be mono')
-
-
-def mult_plot_data(*plot_data: dict[str, list]) -> dict[str:list]:
-    w_arrays: list[list] = []
-    t_arrays: list[list] = []
-    s_arrays: list[list] = []
-    f_arrays: list[list] = []
-    for data in plot_data:
-        for k in data:
-            if k == 'wave_form':
-                w_arrays.append(data[k])
-            elif k == 'seconds':
-                t_arrays.append(data[k])
-            elif k == 'spectrum':
-                s_arrays.append(data[k])
-            elif k == 'frequence':
-                f_arrays.append(data[k])
-    mult_plot_d = {
-        'wave_form': w_arrays,
-        'seconds': t_arrays,
-        'spectrum': s_arrays,
-        'frequence': f_arrays
-    }
-    mult_plot_d = format_data_d(mult_plot_d)
-    return mult_plot_d
-
-def format_data_d(data_d: dict[str, list]) -> dict[str, list]:
-    for k in data_d:
-        if k == 'seconds':
-            arr = max(data_d[k])
-            data_d[k] = arr
-        elif k == 'frequence':
-            arr = max(data_d[k])
-            data_d[k] = arr
-    t_size = len(data_d['seconds'])
-    f_size = len(data_d['frequence'])
-    print(f_size)
-    w_arrays = [resize_arrays(arr,t_size) for arr in data_d['wave_form']]
-    s_arrays = [resize_arrays(arr,f_size) for arr in data_d['spectrum']]
-    print(len(s_arrays[0]))
-    data_d['wave_form'] = w_arrays
-    data_d['spectrum'] = s_arrays
-    return data_d
-
-
-def resize_arrays(array: list, size : int) -> list:
-    dif = size - len(array)
-    z_arr = [0]*dif
-    array.extend(z_arr)
+def resize_arrays(array: list, size: int) -> list:
+    if len(array) < size:
+        dif = size - len(array)
+        z_arr = [0] * dif
+        array.extend(z_arr)
+    if len(array) > size:
+        array = array[:size]
     return array
 
 def get_random_colour() -> tuple:
@@ -90,18 +71,7 @@ def get_random_colour() -> tuple:
     colour=cmap(random.randint(0, 19))
     return colour
 
-def print_comp_graph(arr_dict: dict) -> None:
-    t_array = arr_dict['seconds']
-    f_array = arr_dict['frequence']
-    w_array = arr_dict['wave_form']
-    s_array = arr_dict['spectrum']
-    for arr in w_array:
-        plt.plot(t_array, arr, get_random_colour())
-    for arr in s_array:
-        plt.plot(f_array, arr, get_random_colour())
-    plt.show()
-
-audio_data = signal_plot_data('varios_widgets/IRCwidget/100.wav')
-audio_data_2 = signal_plot_data('varios_widgets/IRCwidget/test.wav')
-comp_data = mult_plot_data(audio_data, audio_data_2)
-print_comp_graph(comp_data)
+audio_data = signal_data('varios_widgets/IRCwidget/100.wav', 'pure 100')
+print(type(audio_data))
+audio_data_2 = signal_data('varios_widgets/IRCwidget/test.wav', 'sweep')
+plot_data(audio_data, audio_data_2)
