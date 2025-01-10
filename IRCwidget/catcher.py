a63ca558021f14ee6461e79176b370167c96bca0
AUSep <anibal.u.sepulveda@gmail.com>
2025-01-09 22:19:07 -0300

    sweep corrected

--- IRCwidget/catcher.py
+++ IRCwidget/catcher.py
@@ -8,7 +8,7 @@ class Catchr():
         self.__chunk = 1024
         self.__format = pyaudio.paFloat32
         self.__channels = 1 if sys.platform == 'darwin' else 2
-        self.__rate = 96000
+        self.__rate = 44100 
     
     @property
     def chunk(self) -> int:
@@ -28,8 +28,8 @@ class Catchr():
     
     def play_sweep(self) -> None:
         t = np.linspace(start=0, num=(self.rate)*5, stop=5, dtype=np.float32)
-        n=(self.rate/2)**(1/5)
-        sweep = 0.5* np.sin(2*np.pi*(n**t)*t)
+        n=(self.rate/50)
+        sweep = 0.25*np.sin(2*np.pi*n*(t**2))
         p = pyaudio.PyAudio()
         o_stream = p.open(format=self.format,
                           channels=self.channels,
