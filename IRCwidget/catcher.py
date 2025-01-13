fd866a22d885f13cf9240541b696e559b4095f95
Aníbal Uriel <anibal.u.sepulveda@gmail.com>
2025-01-13 17:26:50 -0300

    open_port

--- IRCwidget/catcher.py
+++ IRCwidget/catcher.py
@@ -1,14 +1,16 @@
 import sys
-import pyaudio
+import pyaudio as pa
 import numpy as np
 import matplotlib.pyplot as plt
 
 class Catchr():
     def __init__(self):
-        self.__chunk = 1024
-        self.__format = pyaudio.paFloat32
+        self.__chunk = 64
+        self.__format = pa.paFloat32
         self.__channels = 1 if sys.platform == 'darwin' else 2
-        self.__rate = 44100
+        self.__rate = 96000
+        self.__output = pa.PyAudio()
+        self.__input = pa.PyAudio()
     
     @property
     def chunk(self) -> int:
@@ -25,17 +27,41 @@ class Catchr():
     @property
     def rate(self) -> int:
         return self.__rate
+    
+    @property
+    def output(self) -> pa.PyAudio:
+        return self.__output
+    
+    @property
+    def input(self) -> pa.PyAudio:
+        return self.__input
 
     def play(self, signal : np.ndarray) -> None:
-        p = pyaudio.PyAudio()
-        o_stream = p.open(format=self.format,
-                          channels=self.channels,
-                          rate=self.rate,
-                          output=True)
+        o_stream = self.open_port(self.output)
         o_stream.write(signal.tobytes())
         o_stream.close()
-        p.terminate()
+        self.output.terminate()
+    
+    def calibrate(self) -> None:
+        o_stream = self.open_port(self.output)
+        i=0
+        tone = self.tone()
+        while o_stream.is_active():
+            data = tone[i:i+self.chunk]
+            if i<len(tone):
+                i+=self.chunk
+            else:
+                i=0
+            o_stream.write(data)
 
+    def open_port(self, port : pa.PyAudio) -> pa.Stream:
+        stream = port.open(format=self.format,
+                        channels=self.channels,
+                        rate=self.rate,
+                        output=True if port == self.output else False,
+                        input=True if port == self.input else False)
+        return stream
+            
     def sweep(self) -> np.ndarray:
         t = np.linspace(start=0, num=(self.rate)*5, stop=5, dtype=np.float32)
         n=(self.rate/50)
@@ -49,5 +75,11 @@ class Catchr():
         pulse = np.concatenate([zeros, ones, zeros])
         pulse.astype(np.float32)
         return pulse
+    
+    def tone(self) -> np.ndarray:
+        t=np.linspace(start=0, num=self.rate, stop=1, dtype=np.float32)
+        tone =0.25*np.sin(2*np.pi*500*t)
+        return tone
 
-
+a = Catchr()
+a.play(a.tone())
