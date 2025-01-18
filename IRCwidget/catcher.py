2c95847b05b28f7671728f0be9e106e157d3e327
AUSep <anibal.u.sepulveda@gmail.com>
2025-01-18 19:53:18 -0300

    gevent port control

--- IRCwidget/catcher.py
+++ IRCwidget/catcher.py
@@ -1,7 +1,9 @@
 import sys
 import pyaudio as pa
 import numpy as np
+import gevent as gv
 import matplotlib.pyplot as plt
+
 from scipy.signal import chirp, butter, sosfilt
 
 
@@ -13,6 +15,7 @@ class Catchr():
         self.__rate = 44100
         self.__output = pa.PyAudio()
         self.__input = pa.PyAudio()
+        self.__delay = None
         
     @property
     def chunk(self) -> int:
@@ -22,6 +25,10 @@ class Catchr():
     def format(self) -> int:
         return self.__format
     
+    @format.setter
+    def format(bits : int, float : bool = True):
+        pass
+
     @property
     def channels(self) -> int:
         return self.__channels
@@ -38,48 +45,18 @@ class Catchr():
     def input(self) -> pa.PyAudio:
         return self.__input
     
-    def play(self, signal : np.ndarray) -> None:
-        o_stream = self.open_port(self.output)
-        o_stream.write(signal.tobytes())
-        o_stream.close()
-        self.output.terminate()
+    @property
+    def delay(self) -> int:
+        return self.__delay
     
-    def vol_in(self, stream : pa.Stream) -> float:
-        audio_chunk = stream.read(self.chunk)
-        audio_data = np.frombuffer(audio_chunk)
-        sq_sum = np.square(audio_data)
-        rms = np.sqrt(np.mean(sq_sum))
-        print(type(rms))
-        return rms
-
-    def calibrate(self) -> pa.Stream:
-        o_stream = self.open_port(self.output)
-        i_stream = self.open_port(self.input)
-        i=0
-        tone = self.tone()
-        while o_stream.is_active() == True:
-            data = tone[i:i+self.chunk]
-            if i<len(tone):
-                i+=self.chunk
-            else:
-                i=0
-            o_stream.write(data)
-        return o_stream, i_stream
-
-    def open_port(self, port : pa.PyAudio) -> pa.Stream:
-        stream = port.open(format=self.format,
-                        channels=self.channels,
-                        rate=self.rate,
-                        output=True if port == self.output else False,
-                        input=True if port == self.input else False)
-        return stream
-            
-    def sweep(self) -> np.ndarray: 
+    def sweep(self, amp = float) -> np.ndarray: 
         t = np.linspace(start=0, num=self.rate*45, stop=30)
-        sweep = 0.25*chirp(t,10,45,22050,'quadratic')
+        sweep = amp*chirp(t,10,45,22050,'quadratic')
         sos = butter(10, (22000/self.rate), output='sos')
         sweep = sosfilt(sos, sweep)
         sweep = np.array(sweep, dtype=np.float32)
+        plt.plot(t, sweep)
+        plt.show()
         return sweep
     
     def pulse(self) -> np.ndarray:
@@ -90,11 +67,81 @@ class Catchr():
         pulse.astype(np.float32)
         return pulse
     
-    def tone(self) -> np.ndarray:
+    def tone(self, amp = float) -> np.ndarray:
         t=np.linspace(start=0, num=self.rate, stop=1, dtype=np.float32)
-        tone =0.25*np.sin(2*np.pi*100*t)
+        tone =amp*np.sin(2*np.pi*1000*t)
         return tone
+    
+    def open_port(self, port : pa.PyAudio) -> pa.Stream:
+        stream = port.open(format=self.format,
+                        channels=self.channels,
+                        rate=self.rate,
+                        output=True if port == self.output else False,
+                        input=True if port == self.input else False)
+        return stream
+            
+    def play(self, signal : np.ndarray) -> None:
+        o_stream = self.open_port(self.output)
+        o_stream.write(signal.tobytes())
+        o_stream.close()
+        self.output.terminate()
+        gv.sleep(1)
+    
+    def rec(self, len : int) -> np.ndarray:
+        i_stream = self.open_port(self.input)
+        audio_data = i_stream.read(len)
+        audio_data = np.array(audio_data)
+        i_stream.close()
+        self.input.terminate()
+        gv.sleep(1)
+        return audio_data
+    
+    def rms(self, audio_data : np.ndarray) -> float:
+        sq_sum = np.square(audio_data)
+        rms = np.sqrt(np.mean(sq_sum))
+        print(type(rms))
+        return rms
+
+    def vol_in(self, stream : pa.Stream) -> float:
+        audio_chunk = stream.read(self.chunk)
+        audio_data = np.frombuffer(audio_chunk)
+        rms = self.rms(audio_data)
+        return rms
+    
+    def comunicate(self, signal : np.ndarray) -> np.ndarray:
+        play_routine = gv.spawn(self.play(signal))
+        rec_routine = gv.spawn_later(self.delay, self.rec(len(signal)))
+        gv.joinall([play_routine,rec_routine])
+        signal_return = rec_routine.get()
+        return signal_return
+
+    def max_gain_test(self) -> tuple[float,float]:
+        stream = self.open_port(self.input)
+        noise = stream.read(self.rate*5)
+        noise_lvl= self.rms(np.array(noise, dtype=np.float32))
+        max_amp=0
+        while stream.is_active():
+            audio_data
+            tone = self.tone(max_amp)
+            self.play(tone)
+            max_amp+=0.01
+            audio_data = stream.read(len(tone))
+            if self.rms(audio_data) >= 0.9:
+                stream.close()
+        return max_amp, noise_lvl
+
+    def sweep_test(self, max_amp : float) -> dict:
+        amp = 0
+        sweep_returns={}
+        while amp < max_amp:
+            amp+=0.01
+            sweep = self.sweep(amp)
+            self.play(sweep)
+            audio_array = self.comunicate(sweep)
+            sweep_returns[amp] = audio_array
+        return sweep_returns
 
 a = Catchr()
-a.play(a.sweep()) 
+a.play(a.sweep(0.25)) 
+
 
