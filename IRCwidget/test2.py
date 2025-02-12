0b6af62e560af01d634611b706f20f9b16394bc3
AUSep <anibal.u.sepulveda@gmail.com>
2025-02-12 14:42:20 -0300

    find_peaks implemented

--- IRCwidget/test2.py
+++ IRCwidget/test2.py
@@ -4,7 +4,7 @@ from plotter import plot_audio_data, get_spect_data
 
 sample_rate = 44100
 
-t = np.linspace(0,5,sample_rate, dtype=np.float32)
+t = np.linspace(0,1,sample_rate, dtype=np.float32)
 x1 = 0.9*np.sin(2*np.pi*20*t)
 x2 = 0.2*np.sin(2*np.pi*100*t)
 x3 = 0.5*np.sin(2*np.pi*50*t)
@@ -13,9 +13,10 @@ audio_data = (g,sample_rate,sample_rate)
 
 def track_harmonics(signal : np.ndarray, s_rate: int, n = int) -> np.ndarray:
     f_array, spect_array = get_spect_data(signal, s_rate)
-    print(f_array)
     peak_indxs, prop = find_peaks(spect_array, height=0.1)
+    peak_indxs = np.array(peak_indxs, dtype=np.float32)
     peak_hght = prop['peak_heights']
+    peak_hght = np.array(peak_hght, dtype=np.float32)
     peak_dict = dict(zip(peak_hght, peak_indxs))
     i = 0
     indxs = []
@@ -24,8 +25,8 @@ def track_harmonics(signal : np.ndarray, s_rate: int, n = int) -> np.ndarray:
         max_indx = int(peak_dict.pop(max_peak))
         indxs.append(max_indx)
         i+=1
-    print(indxs)
     peak_freqs = [f_array[i] for i in indxs]
+    peak_freqs = np.array(peak_freqs, dtype=np.float32)
     return peak_freqs
 
 print(track_harmonics(g, sample_rate, 3))
