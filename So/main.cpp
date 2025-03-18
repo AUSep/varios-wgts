e35456178717a8ba16e5971c898df6c1f19d8b4b
Aníbal Uriel <anibal.u.sepulveda@gmail.com>
2025-03-18 16:28:56 -0300

    char specttro

--- So/main.cpp
+++ So/main.cpp
@@ -3,9 +3,24 @@
 #include <portaudio.h>
 #include <cstring>
 #include <fftw3.h>
+#include <math.h>
 
-#define SAMPLE_RATE 44100
+#define SAMPLE_RATE 44100.0
 #define FRAMES_PER_BUFFER 512
+#define N_CHANNELS 2
+#define SPECT_LOW_BOUND 20
+#define SPECT_HI_BOUND 22000
+
+typedef struct 
+{
+    double* in;
+    double* out;
+    fftw_plan p;
+    int startIndex;
+    int spectSize;
+}streamCallbackData;
+
+static streamCallbackData* spectData;
 
 /*Error handler por Port Audio*/
 static void checkError(PaError err){
@@ -33,41 +48,57 @@ static inline float max(float a, float b) {
     return a > b ? a : b;
 }
 
+static inline float min(float a, float b) {
+    return a < b ? a : b;
+}
+
 static int paTestCallback(
     const void* inputBuffer, void* outputBuffer, unsigned long framesPerBuffer,
     const  PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlag,
     void* userData
 ) {
 
+    streamCallbackData* callbackData = (streamCallbackData*)userData;
     float* in = (float*) inputBuffer;
     (void)outputBuffer;
 
     int dispSize = 100;
     printf("\r");
 
-    float vol_r = 0;
-    float vol_l = 0;
-    
-    for (unsigned long i = 0; i < framesPerBuffer * 2 ; i += 2 ) {
-        vol_l = max(vol_l, std::abs(in[i]));
-        vol_r = max(vol_r, std::abs(in[i+1]));
+    for (unsigned long i = 0; i < framesPerBuffer; i++) {
+        callbackData->in[i] = in[i * N_CHANNELS];
     }
 
-    for (int i = 0; i < dispSize; i++) {
-        float barProportion = i/(float)dispSize;
-        if (barProportion <= vol_l && barProportion <= vol_r) {
+    fftw_execute(callbackData->p);
+    for (int i = 0; i<dispSize; i++ ) {
+        double proportion = i/dispSize;
+        double freq = callbackData->out[(int)(callbackData->startIndex 
+            + proportion * callbackData->spectSize)];
+
+        if (freq <0.125) {
+            printf("▁");
+        }
+        else if (freq < 0.25){
+            printf("▂");
+        }
+        else if (freq < 0.375){
+            printf("▃");
+        }
+        else if (freq < 0.5){
+            printf("▄");
+        }
+        else if (freq < 0.625){
+            printf("▅");
+        }
+        else if (freq < 0.75){
+            printf("▆");
+        }
+        else if (freq < 0.875){
+            printf("▇");
+        }
+        else {
             printf("█");
         }
-        else if ( barProportion <= vol_l ) {
-            printf("▀");
-        }
-        else if ( barProportion <= vol_r ) {
-            printf("▄");
-         }
-        else {
-            printf(" ");
-        }
-
     }
     fflush(stdout);
 
@@ -78,7 +109,27 @@ int main(){
     PaError err; 
     err = Pa_Initialize();
     checkError(err);
-    
+
+    spectData = (streamCallbackData*)malloc(sizeof(streamCallbackData));
+    spectData->in = (double*)malloc(sizeof(double*) * FRAMES_PER_BUFFER);
+    spectData->out = (double*)malloc(sizeof(double*) * FRAMES_PER_BUFFER);
+
+    if (spectData->in == NULL || spectData->out == NULL) {
+        printf("Could not allocate spectrum data\n");
+        exit(EXIT_FAILURE);
+    }
+    spectData->p = fftw_plan_r2r_1d(FRAMES_PER_BUFFER,
+        spectData->in,
+        spectData->out,
+        FFTW_R2HC,
+        FFTW_ESTIMATE);
+
+    double sampRatio = FRAMES_PER_BUFFER/SAMPLE_RATE;
+    spectData->startIndex = std::ceil(sampRatio * SPECT_LOW_BOUND);
+    spectData->startIndex = min(
+        std::ceil(sampRatio * SPECT_HI_BOUND),
+        FRAMES_PER_BUFFER/2.0) - spectData->startIndex;
+
     displayDeviceInfo();
 
     /*Selects an específic audio device and sets the parameters for the output and input
@@ -112,7 +163,7 @@ int main(){
         FRAMES_PER_BUFFER,
         paNoFlag,
         paTestCallback,
-        NULL
+        spectData
     );
     checkError(err);
 
@@ -126,6 +177,11 @@ int main(){
 
     err = Pa_CloseStream(stream);
     checkError(err);
+
+    fftw_destroy_plan(spectData->p);
+    fftw_free(spectData->in);
+    fftw_free(spectData->out);
+    free(spectData);
 
     /*Ends PortAudio*/
     err = Pa_Terminate();
